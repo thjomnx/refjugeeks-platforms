deefab6b6d74a245b03014ec1d75129d672cf826
refjugeeks Admin <rgeek@mgmt1.refjugeeks.net>
2018-09-08 20:20:48 +0200

    Add 'vsftpd' auto configuration

--- setup/debian9-systemsetup.sh
+++ setup/debian9-systemsetup.sh
@@ -26,13 +26,14 @@ EOF
 chmod 440 /etc/sudoers.d/rgeek
 
 # Grav website
+GRAV_CONFIG="refjugeeks-grav"
 GRAV_WEBROOT="/home/www/html"
 GRAV_USER="www-data"
 GRAV_GROUP="$GRAV_USER"
 
 apt-get install -y apache2 php php-gd php-curl php-xml php-mbstring php-json php-zip php-yaml php-apcu
 
-cat >> /etc/apache2/sites-available/refjugeeks-grav.conf << EOF
+cat >> "/etc/apache2/sites-available/${GRAV_CONFIG}.conf" << EOF
 <VirtualHost *:80>
         ServerAdmin webmaster@localhost
         DocumentRoot $GRAV_WEBROOT
@@ -51,7 +52,7 @@ EOF
 
 a2enmod rewrite
 a2dissite 000-default
-a2ensite refjugeeks-grav
+a2ensite "$GRAV_CONFIG"
 
 systemctl restart apache2
 
@@ -59,14 +60,44 @@ mkdir -p "$GRAV_WEBROOT"
 chown -R "$GRAV_USER":"$GRAV_GROUP" "$GRAV_WEBROOT"
 
 # DNS/DHCP
+DNSMASQ_DEFAULTS="/etc/default/dnsmasq"
+DNSMASQ_CONFIG_D="/etc/dnsmasq.d"
+
 apt-get install -y dnsmasq
 
-sed -i /CONFIG_DIR/ s/$/,.select/ /etc/default/dnsmasq
-cp -f "$RESOURCES/etc/dnsmasq.d/*.select" /etc/dnsmasq.d/
-chown 0:0 /etc/dnsmasq.d/*.select
-(cd /etc/dnsmasq.d && ln -sf refjugeeks-lan.conf.primary.select refjugeeks-lan.conf)
+sed -i /CONFIG_DIR/ s/$/,.select/ "$DNSMASQ_DEFAULTS"
+cp -f "${RESOURCES}${DNSMASQ_CONFIG_D}/*.select" "$DNSMASQ_CONFIG_D"
+chown 0:0 "$DNSMASQ_CONFIG_D/*.select"
+(cd "$DNSMASQ_CONFIG_D" && ln -sf refjugeeks-lan.conf.primary.select refjugeeks-lan.conf)
 
 systemctl restart dnsmasq
 
-# FTP (server)
-apt-get install -y ???
+# FTP
+apt-get install -y vsftpd ftp
+
+VSFTPD_CONFIG="/etc/vsftpd.conf"
+VSFTPD_CONFIG_D="/etc/vsftpd.d"
+VSFTPD_USERLIST="$VSFTPD_CONFIG_D/userlist"
+VSFTPD_USER="rgeekftp"
+
+mkdir -p "$VSFTPD_CONFIG_D"
+
+if ! grep -q "$VSFTPD_USER" "$VSFTPD_USERLIST"
+then
+    echo "$VSFTPD_USER" >> "$VSFTPD_USERLIST"
+fi
+
+if ! grep -q "refjugeeks welcome" "$VSFTPD_CONFIG"
+then
+    cat >> "$VSFTPD_CONFIG" << EOF
+
+# Custom configuration for refjugeeks welcome
+userlist_file=$VSFTPD_USERLIST
+userlist_enable=YES
+userlist_deny=NO
+
+EOF
+fi
+
+systemctl restart vsftpd
+
